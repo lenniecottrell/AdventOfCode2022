1b2c90e8877cd90dd4deb3a57f95ced620e8edb0
lenniecottrell <64614859+lenniecottrell@users.noreply.github.com>
2023-01-10 06:56:36 -0800

    add typescript, add day 4 pt 1 solution

---
+++ Day-4/camp-cleanup_pt1.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "input.txt");
+class SectionRange {
+    constructor(lower, upper) {
+        this.lower = lower;
+        this.upper = upper;
+    }
+}
+fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
+    if (!err) {
+        const allData = data.split("\n");
+        const ParseRanges = (range) => {
+            let singleRange = range.split(",");
+            let firstRangeSplit = singleRange[0].split("-");
+            let secondRangeSplit = singleRange[1].split("-");
+            const rangePair = [];
+            const firstRange = new SectionRange(Number(firstRangeSplit[0]), Number(firstRangeSplit[1]));
+            const secondRange = new SectionRange(Number(secondRangeSplit[0]), Number(secondRangeSplit[1]));
+            rangePair.push(firstRange, secondRange);
+            //console.log(`First range: ${rangePair[0].lower}-${rangePair[0].upper}. Second range: ${rangePair[1].lower}-${rangePair[1].upper}`)
+            return rangePair;
+        };
+        const Contains = (range1, range2) => {
+            return range1.lower <= range2.lower && range1.upper >= range2.upper || range1.lower >= range2.lower && range1.upper <= range2.upper;
+        };
+        let counter = 0;
+        for (let i = 0; i < allData.length; i++) {
+            if (Contains(ParseRanges(allData[i])[0], ParseRanges(allData[i])[1])) {
+                counter++;
+            }
+        }
+        console.log(counter);
+    }
+    else {
+        console.error(err);
+        //it's not 869
+        //550 is too high
+    }
+});
