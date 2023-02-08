e92e158279acd76e73ac28d9a2a3604e9f769b2b
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2023-02-07 21:23:44 -0800

    parse containers and instructions

--- Day-5/supply-stacks_pt1.ts
+++ Day-5/supply-stacks_pt1.ts
@@ -1,3 +1,5 @@
+import { RecordWithTtl } from "dns";
+
 const filesys = require("fs");
 const path_ = require("path");
 
@@ -5,8 +7,7 @@ const file_path = path_.join(__dirname, "example.txt");
 
 filesys.readFile(file_path, { encoding: "utf-8" }, (err: any, data: string) => {
   if (!err) {
-    const firstLine = data.split("\n", 1)[0].split("\r", 1);
-    //represent the supply stacks somehow
+    //represent the supply stacks
     //separate the stacks from the instructions
     //parse the stacks
     //represent the instructions
@@ -23,10 +24,24 @@ filesys.readFile(file_path, { encoding: "utf-8" }, (err: any, data: string) => {
     - in javascript I think I can use an array, but that isn't a good option for other languages because it's not resizable
     -3d array!
     */
+    const firstLine: string[] = data.split("\n", 1)[0].split("\r", 1);
+    //this is just the containers, excluding column numbers;
+    const containers: string[] = data
+      .split("\n", 3)
+      .map((row) => row.split("\r", 1)[0].concat(" "));
 
+    //this is all the instructions
+    const instructions: string[] = data
+      .split("\n")
+      .map((row) => row.split("\r", 1)[0])
+      .filter((row) => row.includes("move"));
+    //console.log(instructions);
+
+    //create a 3d array representing the stacks
+    //using the first line to figure out how many columns there are
     const getStacks = (firstRow: string) => {
       //outer array to hold the stacks
-      let stacks = [];
+      let stacks: string[][] = [];
       let stack = Math.round(firstRow.length / 4);
       //put each individual stack inside
       for (let i = 0; i < stack; i++) {
@@ -35,8 +50,46 @@ filesys.readFile(file_path, { encoding: "utf-8" }, (err: any, data: string) => {
       return stacks;
     };
 
-    const stacks = getStacks(firstLine[0]);
-    console.log(stacks);
+    //returns a 3d array with the right number of columns(stacks)
+    let stacks = getStacks(firstLine[0]);
+
+    //fill each inner array with the boxes
+    //example row: '[Z] [M] [P] '
+    const parseRow = (row: string, stack: string[][]) => {
+      let counter = 0;
+      for (let i = 0; i < row.length; i += 4) {
+        stack[counter].unshift(row.slice(i, i + 4));
+        counter++;
+      }
+      return stack;
+    };
+    for (let i = 0; i < containers.length; i++) {
+      parseRow(containers[i], stacks);
+    }
+    //console.log(stacks);
+
+    const parseInstruction = (row: string) => {
+      const split = row.split(" ");
+      const nums: number[] = split.map((item) => parseInt(item));
+      const vals: number[] = [];
+      for (let i = 1; i < nums.length; i += 2) {
+        vals.push(nums[i]);
+      }
+      return vals;
+    };
+
+    let example = "move 2 from 2 to 1";
+    //console.log(parseInstruction(example));
+
+    const parseInstructions = (rows: string[]) => {
+      const InstructionNums: number[][] = [];
+      for (let i = 0; i < rows.length; i++) {
+        InstructionNums.push(parseInstruction(rows[i]));
+      }
+      return InstructionNums;
+    };
+
+    console.log(parseInstructions(instructions));
   } else {
     console.error(err);
   }
